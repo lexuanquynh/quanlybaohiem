ed419da02a35feb9f2b1d227fdf832e447f73405
xcode vn <xcode vn>
2016-10-30 10:54:17 +0700

    add file

---
+++ quanlybaohiem/quanlybaohiem/Controller/MyDBConnector.vb
@@ -0,0 +1,30 @@
+﻿Imports System.Data.SqlClient
+
+Public Class MyDBConnector
+    'Data Source=BOYALONE;Initial Catalog=QLHDBH;Integrated Security=True
+    'data source=hp-pc;initial catalog=qlhdbh;integrated security=true
+    Protected Friend Const DE_CONNECT_SQL As String = "Data Source=BOYALONE;Initial Catalog=QLHDBH;Integrated Security=True"
+    Dim con As New SqlConnection
+    'Ham tao ket noi toi database
+    Public Function TaoKetNoi() As SqlConnection
+        Try
+            Dim strketnoi As String = DE_CONNECT_SQL
+            con.ConnectionString = strketnoi
+            con.Open()
+            Return con
+        Catch ex As Exception
+            MessageBox.Show("Có lỗi xảy ra. Vui lòng kiểm tra lại kết nối tới Database của bạn!")
+            Return con
+        End Try
+    End Function
+
+    'Ham dong ket noi toi database
+    Public Sub DongKetNoi()
+        Try
+            con.Close()
+            con.Dispose()
+        Catch ex As Exception
+            MessageBox.Show(ex.ToString)
+        End Try
+    End Sub
+End Class
